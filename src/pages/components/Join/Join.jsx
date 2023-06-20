e0dcdad47cc4abbc4b14a64fff19ba95a90bce98
Leonardoguimaraesdev <leonardo.guimaraes.dev@gmail.com>
2023-06-20 19:39:05 -0300

    Css changed

--- src/pages/components/Join/Join.jsx
+++ src/pages/components/Join/Join.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react"
-import io from 'socket.io-client'
+import { useState } from "react";
+import io from "socket.io-client";
+import styles from "../../../styles/Join.module.scss";
 
-export default function Join({setChatVisibility, setSocket}) {
+export default function Join({ setChatVisibility, setSocket }) {
+  const [name, setName] = useState("");
 
-    const [name, setName] = useState('')
+  const handleSubmit = async () => {
+    if (!name.trim()) return;
+    const socket = await io.connect("http://localhost:3001");
+    socket.emit("set_username", name);
+    setSocket(socket);
+    setChatVisibility(true);
+  };
 
-    const handleSubmit = async () => {
-        if(!name.trim()) return
-        const socket = await io.connect('http://localhost:3001')
-        socket.emit('set_username', name)
-        setSocket(socket)
-        setChatVisibility(true)   
-    }
-
-    return (
-        <div>
-            <h1>Join</h1>
-            <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder="Nome de usuário" />
-            <button onClick={() => handleSubmit()}>Entrar</button>
-        </div>
-    )
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h1>Chat em tempo real</h1>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nome de usuário"
+        />
+        <button onClick={() => handleSubmit()}>ENTRAR</button>
+      </div>
+    </div>
+  );
 }
